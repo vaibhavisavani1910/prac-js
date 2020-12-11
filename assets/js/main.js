f13b7b5c0da1bdc71eb8850547795808e0fb7316
Admin <admin@Admins-MacBook-Pro.local>
2020-12-11 13:34:10 +0530

    filter added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 window.onload = () => {
     
 //Add new TO do in the list
-    var mybtn = document.getElementById('ip');
+    mybtn = document.getElementById('ip');
+    todos = document.getElementById('todos');
+    todo_text = document.getElementsByClassName('form-check-label');
     
+
     mybtn.addEventListener( 'submit' ,(e) => {
         e.preventDefault();
-        var task = document.getElementById('task').value.trim;
+        var task = document.getElementById('task').value.trim();
         //alert(task);
 
-        document.getElementById('todos').innerHTML += "<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'> "+ task +" <i class='input-helper'></i></label> </div> <i class='remove fa fa-trash'></i></li>";
+        todos.innerHTML += "<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'> "+ task +" <i class='input-helper'></i></label> </div> <i class='remove fa fa-trash'></i></li>";
     });
 
 //dlt or cut-out the clicked task 
@@ -31,6 +34,38 @@ window.onload = () => {
 
 
     });
+
+//Search Logic
+
+    const search = document.getElementById('sip');
+
+    const getList = term => {
+
+        //console.log(todo_text)
+
+        //add class which include searched term 
+        Array.from(todo_text)
+            .filter(todo => !todo.textContent.includes(term))
+                .forEach( todo => {
+                    todo.parentElement.parentElement.classList.add('filtered')
+                })
+
+
+        //remove class which do not include searched term
+        Array.from(todo_text)
+        .filter(todo => todo.textContent.includes(term))
+            .forEach( todo => {
+                todo.parentElement.parentElement.classList.remove('filtered')
+            })
+
+
+    };
+
+    search.addEventListener('keyup', (e) =>{
+        term = search.value.trim();
+        getList(term);
+    } )
+
 
 
 }
